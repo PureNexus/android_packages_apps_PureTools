PureNexusProject-Legacy/android_packages_apps_PureNexusSettings

The Repurposement Part 1

Android.mk
@@ -30,7 +30,7 @@ res_dirs := res $(appcompat_dir) $(cardview_dir) $(recyclerview_dir) $(design_di
 # Build APK
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := PureNexusSettings
+LOCAL_PACKAGE_NAME := PureNexusUtils
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_ENABLED := disabled

src/com/android/purenexusutils/DeviceFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.purenexusutils;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;

src/com/android/purenexusutils/EditPropFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.purenexusutils;
 
 import android.app.Activity;
 import android.app.AlertDialog;

src/com/android/purenexusutils/FiSwitchFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.purenexusutils;
 
 import android.app.Fragment;
 import android.content.ComponentName;

src/com/android/purenexusutils/preferences/ColorPickerPanelView.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings.preferences;
+package com.android.purenexusutils.preferences;
 
 import android.content.Context;
 import android.graphics.Canvas;

src/com/android/purenexusutils/utils/SlidingTabStrip.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings.utils;
+package com.android.purenexusutils.utils;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -25,7 +25,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.android.purenexussettings.R;
+import com.android.purenexusutils.R;
 
 
 class SlidingTabStrip extends LinearLayout {

PureNexus/android_packages_apps_PureTools

Android.mk
@@ -30,7 +30,7 @@ res_dirs := res $(appcompat_dir) $(cardview_dir) $(recyclerview_dir) $(design_di
 # Build APK
 include $(CLEAR_VARS)
 
-LOCAL_PACKAGE_NAME := PureNexusSettings
+LOCAL_PACKAGE_NAME := PureTools
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_ENABLED := disabled

src/com/android/puretools/DeviceFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.puretools;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;

src/com/android/puretools/EditPropFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.puretools;
 
 import android.app.Activity;
 import android.app.AlertDialog;

src/com/android/puretools/FiSwitchFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings;
+package com.android.puretools;
 
 import android.app.Fragment;
 import android.content.ComponentName;

src/com/android/puretools/preferences/ColorPickerPanelView.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings.preferences;
+package com.android.puretools.preferences;
 
 import android.content.Context;
 import android.graphics.Canvas;

src/com/android/puretools/utils/SlidingTabStrip.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.android.purenexussettings.utils;
+package com.android.puretools.utils;
 
 import android.content.Context;
 import android.graphics.Canvas;
@@ -25,7 +25,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.android.purenexussettings.R;
+import com.android.puretools.R;
 
 
 class SlidingTabStrip extends LinearLayout {